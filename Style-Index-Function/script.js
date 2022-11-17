ce1384c43a0242a55e5085c5604e8cac11c5eb2a
John Maybee <john.d.maybee@gmail.com>
2022-11-17 17:02:33 -0500

    added quite a bit of the html content that will be needed. also asked a question in script

--- Style-Index-Function/script.js
+++ Style-Index-Function/script.js
@@ -1,6 +1,8 @@
 console.log('Hello, Earthlings!');
 
 $(document).ready(onReady);
+// am I supposed to know what the total monthly cost comes from? I've figured out that it's 1/12 of the annual salary.
+
 
 function onReady() {
     console.log('in onReady ~(_:(1)');
