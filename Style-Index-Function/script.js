abf381f4b2e8b7ef212527633e97e7161dca640f
John Maybee <john.d.maybee@gmail.com>
2022-11-19 22:05:46 -0500

    got first name to populate in the domgit add .git add .

--- Style-Index-Function/script.js
+++ Style-Index-Function/script.js
@@ -12,9 +12,23 @@ function onReady() {
     // need an .on 'click' created for the deletebtn
 } // end onReady function
 
+//////////////////////////////////////////////addEmployee//////////////////////////////////////////////
+
 function addEmployee() {
     console.log('in addEmployee!');
     // need to append the information from the boxes to the end of the table in the DOM.
     // will end up removing the table I previously entered in the dom and enter it here instead.
-    
+    let firstName = $('#firstNameIn').val();
+    $('#employeeTable').append(`
+        <tr style="height: 50px">
+            <td>${firstName}</td>
+            <td>last name</td>
+            <td>id#</td>
+            <td>position</td>
+            <td>$</td>
+            <td></td>
+        </tr>`
+    );
 } // end addEmployee
+
+// Don't forget to clear the input boxes after you add the employee
