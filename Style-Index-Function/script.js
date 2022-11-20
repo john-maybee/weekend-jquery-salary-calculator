1e38b340cf9b4faa3e0dcef9c81a182cd06fd192
John Maybee <john.d.maybee@gmail.com>
2022-11-19 22:22:36 -0500

    got each of the input fields to translate to the table

--- Style-Index-Function/script.js
+++ Style-Index-Function/script.js
@@ -19,14 +19,19 @@ function addEmployee() {
     // need to append the information from the boxes to the end of the table in the DOM.
     // will end up removing the table I previously entered in the dom and enter it here instead.
     let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let idNum = $('#idIn').val();
+    let position = $('#jobTitleIn').val();
+    let salary = $('#annualSalaryIn').val();
+
     $('#employeeTable').append(`
         <tr style="height: 50px">
             <td>${firstName}</td>
-            <td>last name</td>
-            <td>id#</td>
-            <td>position</td>
-            <td>$</td>
-            <td></td>
+            <td>${lastName}</td>
+            <td>${idNum}</td>
+            <td>${position}</td>
+            <td>$${salary}</td>
+            <td><button class="delete">Delete</button></td>
         </tr>`
     );
 } // end addEmployee
